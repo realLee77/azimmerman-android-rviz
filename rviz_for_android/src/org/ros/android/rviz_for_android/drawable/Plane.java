c8e795e0b2018725b63d7b68042021e7ca8d3e8c
Adam Zimmerman <azimmerman@willowgarage.com>
2012-06-28 17:50:16 -0700

    Fixed map layer texture bug. Map layer now supports nonsquare tiles

--- rviz_for_android/src/org/ros/android/rviz_for_android/drawable/Plane.java
+++ rviz_for_android/src/org/ros/android/rviz_for_android/drawable/Plane.java
@@ -67,10 +67,9 @@ public class Plane extends TexturedTrianglesShape {
 
 	@Override
 	public void draw(GL10 gl) {
+		// Set the texture filtering to use the nearest pixel to disable linear smoothing
 	    gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
 	    gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_NEAREST);
-	    gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
-	    gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_CLAMP_TO_EDGE);
 		super.draw(gl);
 	}
 
