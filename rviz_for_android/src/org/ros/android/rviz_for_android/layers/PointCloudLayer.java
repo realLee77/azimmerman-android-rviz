470b91528e196348c607db846f6926dc14c2a3ef
Adam Zimmerman <azimmerman@willowgarage.com>
2012-07-12 14:49:08 -0700

    Added point cloud with selectable coloring mode

--- rviz_for_android/src/org/ros/android/rviz_for_android/layers/PointCloudLayer.java
+++ rviz_for_android/src/org/ros/android/rviz_for_android/layers/PointCloudLayer.java
@@ -18,18 +18,16 @@ package org.ros.android.rviz_for_android.layers;
 
 import geometry_msgs.Point32;
 
-import java.nio.FloatBuffer;
+import java.util.Random;
 
 import javax.microedition.khronos.opengles.GL10;
 
 import org.ros.android.renderer.Camera;
-import org.ros.android.renderer.Vertices;
 import org.ros.android.renderer.VisualizationView;
 import org.ros.android.renderer.layer.SubscriberLayer;
 import org.ros.android.renderer.layer.TfLayer;
 import org.ros.android.renderer.shapes.Color;
-import org.ros.android.rviz_for_android.drawable.GLSLProgram;
-import org.ros.android.rviz_for_android.drawable.GLSLProgram.ShaderVal;
+import org.ros.android.rviz_for_android.drawable.PointCloudGL;
 import org.ros.android.rviz_for_android.prop.BoolProperty;
 import org.ros.android.rviz_for_android.prop.ColorProperty;
 import org.ros.android.rviz_for_android.prop.LayerWithProperties;
@@ -46,46 +44,20 @@ import org.ros.node.topic.Subscriber;
 import org.ros.rosjava_geometry.FrameTransformTree;
 
 import sensor_msgs.PointCloud;
-import android.opengl.GLES20;
-import android.opengl.Matrix;
 import android.os.Handler;
 import android.util.Log;
 
 public class PointCloudLayer extends SubscriberLayer<sensor_msgs.PointCloud> implements LayerWithProperties, TfLayer {
 
 	private BoolProperty prop;
-
-	private FloatBuffer verticesBuffer;
 	private int pointCount = -1;
-	private boolean readyToDraw = false;
 	private GraphName frame;
 
 	private ConnectedNode connectedNode;
 	private MessageListener<PointCloud> subListener;
 	private Subscriber<sensor_msgs.PointCloud> sub;
-	private float[] color = new float[4];
 	
-	private static final String vShader = 
-			"precision mediump float; 	\n" +
-			"uniform mat4 uMvp;			\n" +
-			"attribute vec4 aPosition;	\n" +
-			"varying vec4 vColor;		\n" +
-			"void main() {				\n" +
-		    "	gl_Position = uMvp * vec4(aPosition.xyz, 1.0);	\n"+
-		    "	vColor = vec4(0.5,0.6,0.9,1.0);					\n"+
-		    "	gl_PointSize = 2.0;								\n"+
-		    "}";
-	
-	private static final String fShader = 
-			"precision mediump float; 	\n" +
-			"varying vec4 vColor;		\n" +
-			"void main()				\n" + 
-			"{							\n" +
-		    "	gl_FragColor = vColor;	\n" +
-			"}";
-
-	private GLSLProgram pcShader;
-	private int[] uniformHandles;
+	private PointCloudGL pc;
 
 	@Override
 	public void onStart(ConnectedNode connectedNode, Handler handler, FrameTransformTree frameTransformTree, Camera camera) {
@@ -104,78 +76,38 @@ public class PointCloudLayer extends SubscriberLayer<sensor_msgs.PointCloud> imp
 					vertices[i++] = p.getY();
 					vertices[i++] = p.getZ();
 				}
-				
-				readyToDraw = false;
-				verticesBuffer = Vertices.toFloatBuffer(vertices);
+
+				pc.setData(vertices);
 				Log.i("PCL", "Done copying. Time: " + (System.nanoTime() - now) / 1000000000.0);
 
 				if(frame == null || !frame.equals(msg.getHeader().getFrameId()))
 					frame = new GraphName(msg.getHeader().getFrameId());
-				readyToDraw = true;
 			}
 		};
 
 		sub.addMessageListener(subListener);
 		this.connectedNode = connectedNode;
 	}
-
-	private boolean shadersLoaded = false;
 	
 	@Override
 	public void draw(GL10 glUnused) {
-		if(!shadersLoaded) {
-			// Init shaders
-			pcShader = new GLSLProgram(vShader,fShader);
-			pcShader.setAttributeName(ShaderVal.POSITION, "aPosition");
-			pcShader.setAttributeName(ShaderVal.MVP_MATRIX, "uMvp");
-			pcShader.compile(glUnused);
-			uniformHandles = pcShader.getUniformHandles();
-			shadersLoaded = true;
-		}
-		if(readyToDraw) {
-			pcShader.use(glUnused);
-
-			GLES20.glEnableVertexAttribArray(ShaderVal.POSITION.loc);
-			GLES20.glVertexAttribPointer(ShaderVal.POSITION.loc, 3, GLES20.GL_FLOAT, false, 12, verticesBuffer);
-			
-			calcMVP();
-			GLES20.glUniformMatrix4fv(getUniform(ShaderVal.MVP_MATRIX), 1, false, MVP, 0);
-	
-			GLES20.glDrawArrays(GLES20.GL_POINTS, 0, pointCount);
-		}
+		pc.draw(glUnused);
 	}
 	
-	protected float[] MVP = new float[16];
-	
-	protected void calcMVP() {
-		Matrix.multiplyMM(MVP, 0, camera.getViewMatrix(), 0, camera.getModelMatrix(), 0);
-		Matrix.multiplyMM(MVP, 0, camera.getViewport().getProjectionMatrix(), 0, MVP, 0);
-	}
-	
-	private int getUniform(ShaderVal s) {
-		return uniformHandles[s.loc];
-	}
-
-	private static enum ColorModes {Flat("Flat color"), GradientX("Gradient X"), GradientY("Gradient Y"), GradientZ("Gradient Z");
-		private String name;
-		ColorModes(String name) {
-			this.name = name;
+	private float[] generateTestData() {
+		Random rand = new Random();
+		int pointCount = rand.nextInt(5000) + 5000;
+		float[] retval = new float[pointCount * 3];
+		
+		for(int i = 0; i < retval.length; i += 3) {
+			retval[i] = rand.nextFloat()*8 - 4;
+			retval[i+1] = rand.nextFloat()*8 - 4;
+			retval[i+2] = retval[i+2] = rand.nextFloat()*4 - 2;
 		}
 		
-		@Override
-		public String toString() {
-			return name;
-		}
+		return retval;		
 	}
-	private static final String[] colorModes;
-	static {
-		colorModes = new String[ColorModes.values().length];
-		int idx = 0;
-		for(ColorModes cm : ColorModes.values()) {
-			colorModes[idx++] = cm.toString();
-		}
-	}
-	
+
 	public PointCloudLayer(Camera cam, GraphName topicName, String messageType) {
 		super(topicName, messageType, cam);
 		prop = new BoolProperty("Enabled", true, null);
@@ -195,19 +127,21 @@ public class PointCloudLayer extends SubscriberLayer<sensor_msgs.PointCloud> imp
 		prop.addSubProperty(new ColorProperty("Flat Color", new Color(1f,1f,1f,1f), new PropertyUpdateListener<Color>() {
 			@Override
 			public void onPropertyChanged(Color newval) {
-				color[0] = newval.getRed();
-				color[1] = newval.getGreen();
-				color[2] = newval.getBlue();
-				color[3] = newval.getAlpha();
+				pc.setColor(newval);
 			}
 		}));
 		
 		prop.addSubProperty(new ListProperty("Color Mode", 0, new PropertyUpdateListener<Integer>() {
 			@Override
 			public void onPropertyChanged(Integer newval) {
-				// TODO: Change which shader is used
+				pc.setColorMode(newval);
 			}
-		}).setList(colorModes));
+		}).setList(PointCloudGL.colorModeNames));
+	
+		pc = new PointCloudGL(cam);
+		pc.setData(generateTestData());
+		pc.setColor(prop.<ColorProperty>getProperty("Flat Color").getValue());
+		pc.setColorMode(1);
 	}
 
 	private void clearSubscriber() {
@@ -233,6 +167,7 @@ public class PointCloudLayer extends SubscriberLayer<sensor_msgs.PointCloud> imp
 	@Override
 	public void onShutdown(VisualizationView view, Node node) {
 		super.onShutdown(view, node);
+		
 		// Clear the subscriber
 		getSubscriber().removeMessageListener(subListener);
 	}
