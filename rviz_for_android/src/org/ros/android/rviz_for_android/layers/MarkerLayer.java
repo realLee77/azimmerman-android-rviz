904bc97207890164aa818658bfbd86041995e1dc
Adam Zimmerman <azimmerman@willowgarage.com>
2012-07-24 17:18:33 -0700

    Improved marker layer

--- rviz_for_android/src/org/ros/android/rviz_for_android/layers/MarkerLayer.java
+++ rviz_for_android/src/org/ros/android/rviz_for_android/layers/MarkerLayer.java
@@ -16,6 +16,7 @@
  */
 package org.ros.android.rviz_for_android.layers;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -46,7 +47,8 @@ public class MarkerLayer extends EditableStatusSubscriberLayer<visualization_msg
 	private FrameTransformTree ftt;
 	private long nextPruneTime;
 	private static final long PRUNE_PERIOD = 1000; // Milliseconds
-	
+	private Object lockObj = new Object();
+
 	public MarkerLayer(GraphName topicName, String messageType, String messageTypeName, Camera cam) {
 		super(topicName, messageType, messageTypeName, cam);
 		nextPruneTime = System.currentTimeMillis() + PRUNE_PERIOD;
@@ -55,55 +57,63 @@ public class MarkerLayer extends EditableStatusSubscriberLayer<visualization_msg
 	@Override
 	public void onMessageReceived(Marker msg) {
 		super.onMessageReceived(msg);
-		
+
 		String ns = msg.getNs();
 		int id = msg.getId();
-		
-		switch(msg.getAction()) {
-		case Marker.ADD:
-			Log.i("MarkerLayer", "Adding marker " + ns + ":" + id);
-			if(!markers.containsKey(ns))
-				markers.put(ns, new HashMap<Integer, MarkerObj>());
-			markers.get(ns).put(id, new MarkerObj(msg, super.camera));
-			break;
-		case Marker.DELETE:
-			Log.i("MarkerLayer", "Deleting marker " + ns + ":" + id);
-			if(markers.containsKey(ns))
-				markers.get(ns).remove(id);
-			break;
-		default:
-			Log.e("MarkerLayer", "Received a message with action " + msg.getAction());
-			return;
-		}
-	}
-	
-	@Override
-	public void draw(GL10 glUnused) {
-		for(HashMap<Integer, MarkerObj> hm : markers.values()) {
-			for(MarkerObj mo : hm.values()) {
-				camera.pushM();
-				if(mo.getFrame() != null)
-					camera.applyTransform(ftt.newTransformIfPossible(camera.getFixedFrame(), mo.getFrame()));
-				mo.shape.draw(glUnused);
-				camera.popM();
+
+		synchronized(lockObj) {
+			switch(msg.getAction()) {
+			case Marker.ADD:
+				Log.i("MarkerLayer", "Adding marker " + ns + ":" + id);
+				if(!markers.containsKey(ns))
+					markers.put(ns, new HashMap<Integer, MarkerObj>());
+				markers.get(ns).put(id, new MarkerObj(msg, super.camera));
+				break;
+			case Marker.DELETE:
+				Log.i("MarkerLayer", "Deleting marker " + ns + ":" + id);
+				if(markers.containsKey(ns))
+					markers.get(ns).remove(id);
+				break;
+			default:
+				Log.e("MarkerLayer", "Received a message with action " + msg.getAction());
+				return;
 			}
 		}
-		if(System.currentTimeMillis() >= nextPruneTime)
-			pruneMarkers();
 	}
-	
+
+	@Override
+	public void draw(GL10 glUnused) {
+		synchronized(lockObj) {
+			for(HashMap<Integer, MarkerObj> hm : markers.values()) {
+				for(MarkerObj mo : hm.values()) {
+					camera.pushM();
+					if(mo.getFrame() != null)
+						camera.applyTransform(ftt.newTransformIfPossible(camera.getFixedFrame(), mo.getFrame()));
+					camera.scaleM(mo.getScale()[0], mo.getScale()[1], mo.getScale()[2]);
+					mo.shape.draw(glUnused);
+					camera.popM();
+				}
+			}
+			if(System.currentTimeMillis() >= nextPruneTime)
+				pruneMarkers();
+		}
+	}
+
 	private void pruneMarkers() {
+		Log.d("MarkerLayer", "Pruning...");
 		for(HashMap<Integer, MarkerObj> hm : markers.values()) {
 			List<Integer> removeIds = new LinkedList<Integer>();
 			for(Integer i : hm.keySet())
 				if(hm.get(i).isExpired())
 					removeIds.add(i);
-			for(Integer i : removeIds)
+			for(Integer i : removeIds) {
 				hm.remove(i);
+				Log.d("MarkerLayer", "Pruned ID " + i);
+			}
 		}
 		nextPruneTime += PRUNE_PERIOD;
 	}
-	
+
 	@Override
 	public void onStart(ConnectedNode connectedNode, Handler handler, FrameTransformTree frameTransformTree, Camera camera) {
 		super.onStart(connectedNode, handler, frameTransformTree, camera);
@@ -119,24 +129,24 @@ public class MarkerLayer extends EditableStatusSubscriberLayer<visualization_msg
 	protected String getMessageFrameId(Marker msg) {
 		return msg.getHeader().getFrameId();
 	}
-	
+
 	private static class MarkerObj {
 		private BaseShape shape;
 		private GraphName frame;
 		private Color color;
-		private long createTime;
+		private long endTime;
 		private int duration;
 		private float[] scale;
 		private int type;
-		
+
 		public MarkerObj(Marker msg, Camera cam) {
 			this.type = msg.getType();
-			this.frame = msg.getFrameLocked()?GraphName.of(msg.getHeader().getFrameId()):null;
-			this.scale = new float[] { (float) msg.getScale().getX(), (float) msg.getScale().getY(), (float) msg.getScale().getZ()};
+			this.frame = msg.getFrameLocked() ? GraphName.of(msg.getHeader().getFrameId()) : null;
+			this.scale = new float[] { (float) msg.getScale().getX(), (float) msg.getScale().getY(), (float) msg.getScale().getZ() };
 			this.duration = msg.getLifetime().secs * 1000;
-			this.createTime = System.currentTimeMillis();
+			this.endTime = System.currentTimeMillis() + duration;
 			this.color = new Color(msg.getColor().getR(), msg.getColor().getG(), msg.getColor().getB(), msg.getColor().getA());
-	
+
 			switch(type) {
 			case Marker.CUBE:
 				shape = new Cube(cam);
@@ -150,20 +160,24 @@ public class MarkerLayer extends EditableStatusSubscriberLayer<visualization_msg
 			}
 
 			shape.setColor(this.color);
-			shape.setTransform(Transform.newFromPoseMessage(msg.getPose()));
+			Transform shapeTrans = Transform.newFromPoseMessage(msg.getPose());
+			Log.i("MarkerLayer", "Shape transform: " + shapeTrans);
+			Log.i("MarkerLayer", "Shape scale: " + Arrays.toString(scale));
+			Log.i("MarkerLayer", "Shape duration: " + duration + " expires at " + endTime);
+			shape.setTransform(shapeTrans);
 		}
-		
+
 		public GraphName getFrame() {
 			return frame;
 		}
-		
+
 		public boolean isExpired() {
 			if(duration == 0)
 				return false;
 			else
-				return (System.currentTimeMillis() - createTime) < duration;
+				return System.currentTimeMillis() > endTime;
 		}
-		
+
 		public float[] getScale() {
 			return scale;
 		}
